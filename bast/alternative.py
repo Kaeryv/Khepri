02c6935458a8b05fe1d230c0ac0334012fe2824c
Kaeryv <kaeryv.alden@gmail.com>
2024-01-12 16:12:33 +0100

    Improvements to alternative form.

--- bast/alternative.py
+++ bast/alternative.py
@@ -2,6 +2,11 @@
     Alternative formulation of RCWA
 
     Work in progress, use with caution.
+
+    Notations:
+    S11 = S[0,0]
+    S22 = S[1,1]
+    S = [[S11, S12], [S21, S22]]
 """
 import numpy as np
 from math import prod, floor, sqrt
@@ -11,10 +16,20 @@ from cmath import pi
 from .constants import c
 from .tools import convolution_matrix
 from scipy.linalg import expm
+from .tools import rotation_matrix
+
+
+def scat_base_transform(S, U):
+    S[0,0] = U @ S[0,0] @ inv(U)
+    S[0,1] = U @ S[0,1] @ inv(U)
+    S[1,0] = U @ S[1,0] @ inv(U)
+    S[1,1] = U @ S[1,1] @ inv(U)
+    return S
 
 def redheffer_product(SA, SB):
-    D = SA[0,1] @ np.linalg.inv(np.eye(SA[0,0].shape[0],dtype=np.complex128) - SB[0,0] @ SA[1,1])
-    F = SB[1,0] @ np.linalg.inv(np.eye(SA[0,0].shape[0],dtype=np.complex128) - SA[1,1] @ SB[0,0])
+    I = np.eye(SA[0,0].shape[0], dtype=np.complex128)
+    D = SA[0,1] @ inv(I - SB[0,0] @ SA[1,1])
+    F = SB[1,0] @ inv(I - SA[1,1] @ SB[0,0])
     
     S11 = SA[0, 0] + D @ SB[0, 0] @ SA[1, 0];
     S12 = D @ SB[0, 1];
@@ -43,7 +58,7 @@ def scattering_relection(KX, KY, W0, V0):
     B = inv(W0) @ Wref - inv(V0) @ Vref
     S11 = -inv(A) @ B
     S12 = 2 * inv(A)
-    S21 = 0.5 * (A-B@inv(A) @B)
+    S21 = 0.5 * (A - B @ inv(A) @ B)
     S22 = B @ inv(A)
     return np.array([[S11, S12], [S21, S22]])
 
@@ -58,31 +73,74 @@ def free_space_eigenmodes(KX, KY):
     V0 = Q @ W0 @ np.diag(1./ csqrt(lam2))
     return W0, V0
 
+def kz_from_kplanar(kx, ky, k0, epsilon):
+    return np.conj(csqrt(k0**2*epsilon-kx**2-ky**2))
+
+def generate_expansion_vectors(pw, a):
+    M = (pw[0] - 1) // 2
+    m = np.arange(-M, M+1)
+    gx = 2 * pi * m / a
+    gy = 2 * pi * m / a
+    gx, gy= np.meshgrid(gx, gy)
+    return - gx.flatten(), - gy.flatten()
+
 class Lattice:
-    def __init__(self, pw, a,  wavelength, kp=(0,0)):
+    def __init__(self, pw, a,  wavelength, kp=(0,0), rotation=0, compute_eigenmodes=True):
         self.pw = pw
-        kx0, ky0 = kp
-        self.W0 = None
-        self.V0 = None
-        M = (pw[0]-1)//2
-        m = np.arange(-M, M+1)
-        kx = kx0 - 2 * pi * m / a
-        ky = ky0 - 2 * pi * m / a
-        self.KX, self.KY = np.meshgrid(kx, ky)
+        self.kp = kp
         self.k0 = 2 * pi / wavelength
-        k0 = self.k0
+        self.epsi = 1
 
-        epsi = 1
-        self.KZ = np.conj(csqrt(k0**2*epsi-self.KX**2-self.KY**2))
+        self.gx, self.gy = generate_expansion_vectors(pw, a)
 
-        self.KX = np.diag(self.KX.flatten()) / k0
-        self.KY = np.diag(self.KY.flatten()) / k0
-        self.KZ = np.diag(self.KZ.flatten()) / k0
+        if rotation != 0:
+            R = rotation_matrix(rotation)
+            self.gx, self.gy = R @ self.g_vectors
 
-        KX = self.KX
-        KY = self.KY
+        self.kx, self.ky = kp[0] + self.gx, kp[1] + self.gy
+
+
+        self.kz = kz_from_kplanar(self.kx, self.ky, self.k0, self.epsi)
+
+        # Normalize wrt k0 (magnitude of incident k-vector) and create matrices
+        self.KX = np.diag(self.kx / self.k0) 
+        self.KY = np.diag(self.ky / self.k0) 
+        self.KZ = np.diag(self.kz / self.k0)
+
         # Eigen modes of free space
-        self.W0, self.V0 = free_space_eigenmodes(KX, KY)
+        if compute_eigenmodes:
+            self.W0, self.V0 = free_space_eigenmodes(self.KX, self.KY)
+
+    @property
+    def g_vectors(self):
+        return np.vstack((self.gx, self.gy))
+
+    def __add__(self, rhs):
+        pw = self.pw
+        g = np.zeros((pw[0]**2, pw[1]**2, 2), dtype=np.complex128)
+        
+        g2 = np.array(list(rhs.g_vectors.T))
+        for i, g1 in enumerate(self.g_vectors.T):
+            g[i, :, :] = g1.reshape(1, 2) + g2
+        
+        l = Lattice((self.pw[0]**2, self.pw[1]**2), 1e-7, 2*np.pi/self.k0, (0,0), 0, False)
+        l.gx = g[:, :, 0].flatten()
+        l.gy = g[:, :, 1].flatten()
+
+        l.kx, l.ky = self.kp[0] - l.gx, self.kp[1] - l.gy
+
+
+        l.kz = kz_from_kplanar(l.kx, l.ky, l.k0, self.epsi)
+
+        # Normalize wrt k0 (magnitude of incident k-vector) and create matrices
+        l.KX = np.diag(l.kx / l.k0) 
+        l.KY = np.diag(l.ky / l.k0) 
+        l.KZ = np.diag(l.kz / l.k0)
+
+        # Eigen modes of free space
+        l.W0, l.V0 = free_space_eigenmodes(l.KX, l.KY)
+        return l
+
 
 def scattering_layer(lattice, eps, depth=400e-9):
     KX = lattice.KX
