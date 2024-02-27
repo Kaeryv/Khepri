7d7a2acbde151c2b6502a5d9ed688b85ee3dc760
Kaeryv <kaeryv.alden@gmail.com>
2024-02-27 19:48:46 +0100

    More

---
+++ examples/fan_extension/twisted_fields.py
@@ -0,0 +1,134 @@
+import sys
+sys.path.append(".")
+
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument("-id", type=int, required=True)
+parser.add_argument("-freq", type=float, required=True)
+parser.add_argument("-angle", type=float, required=True)
+parser.add_argument("-polar", type=str, default="RCP")
+args = parser.parse_args()
+
+from bast.alternative import scattering_structured_layer, Lattice, scat_base_transform,redheffer_product,incident,scattering_reflection,scattering_transmission, scattering_uniform_layer, poynting_fluxes
+from bast.tools import c
+import numpy as np
+from bast.tools import joint_subspace
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from itertools import product
+from bast.misc import block_split
+from bast.fields import translate_mode_amplitudes, fourier_fields_from_mode_amplitudes, fourier2direct,fourier2direct2
+
+
+pw = (5,5)
+a = 1
+kpinc = (0,0)
+
+# Define the structure
+N = 256
+eps = 4*np.ones((N,N))
+x = np.linspace(-0.5, 0.5, N, endpoint=True)
+y = np.linspace(-0.5, 0.5, N, endpoint=True)
+X, Y = np.meshgrid(x, y)
+eps[np.sqrt(X**2+Y**2)<= 0.25] = 1.0
+
+def get_layer_smatrix(l):
+    S, WL, VL, LL = scattering_structured_layer(l, eps, 0.2 * a, return_eigenspace=True)
+    #Si = scattering_uniform_layer(l, 1.0, 0.3 * a)
+    #S = redheffer_product(S, Si)
+    S = np.asarray(np.bmat([ list(s) for s in list(S) ]))
+    return S, WL, VL, LL, l.W0, l.V0
+def get_interlayer_smatrix(l):
+    S, WL, VL, LL = scattering_uniform_layer(l, 1.0, 0.3 * a, return_eigenspace=True)
+    S = np.asarray(np.bmat([ list(s) for s in list(S) ]))
+    return S, WL, VL, LL, l.W0, l.V0
+
+def matrix_r(Ws, Vs, mode):
+    return block_split(joint_subspace([np.block([[w, w],[-v, v]]) for w, v in zip(Ws, Vs)], mode))
+def forall_gs(pw, a, wl, alpha, gs, mode=None, interlayer=False):
+    Ss = list()
+    W0s = list()
+    V0s = list()
+    VLs = list()
+    WLs = list()
+    LLs = list()
+    for kp in gs.T:
+        l = Lattice(pw, a, wl, kp, rotation=alpha)
+        if not interlayer:
+            Sl, WL, VL, LL, W0, V0 = get_layer_smatrix(l)
+        else:
+            Sl, WL, VL, LL, W0, V0 = get_interlayer_smatrix(l)
+        Ss.append(Sl)
+        W0s.append(W0)
+        V0s.append(V0)
+        WLs.append(WL)
+        VLs.append(VL)
+        LLs.append(LL)
+    
+    R0 = matrix_r(W0s, V0s, mode)
+    RL = matrix_r(WLs, VLs, mode)
+    LL = matrix_r(LLs, LLs, mode)
+    return block_split(joint_subspace(Ss, mode)), R0, RL, LL[0,0]
+
+
+
+def compute_source(sl, wl):
+    epsi=1
+    k0 = 2*np.pi/wl
+    #kzi = np.conj(csqrt(k0**2*epsi-kpinc[0]**2-kpinc[1]**2))
+    kzi = k0
+    if args.polar =="RCP":
+        esrc = incident(sl.pw, 1, 1j, kp=(sl.kp[0], sl.kp[1], kzi))
+    elif args.polar == "LCP":
+        esrc = incident(sl.pw, 1, -1j, kp=(sl.kp[0], sl.kp[1], kzi))
+    elif args.polar == "S":
+        esrc = incident(sl.pw, 1, 0, kp=(sl.kp[0], sl.kp[1], kzi))
+    elif args.polar == "P":
+        esrc = incident(sl.pw, 0, 1, kp=(sl.kp[0], sl.kp[1], kzi))
+    elif args.polar == "SP":
+        esrc = incident(sl.pw, 1, 1, kp=(sl.kp[0], sl.kp[1], kzi))
+    else:
+        assert(False)
+    return esrc
+
+def compute_field_slice(alpha, f):
+    wl = c / f
+    print(wl)
+    g1s = Lattice(pw, a, wl, kpinc, rotation=0).g_vectors
+    g2s = Lattice(pw, a, wl, kpinc, rotation=alpha).g_vectors
+    
+    S1, R01, RL1, L1 = forall_gs(pw, a, wl, 0,     g2s, mode=1)
+    Si, R0i, Ri,  Li = forall_gs(pw, a, wl, alpha, g1s, mode=1, interlayer=True)
+    S2, R02, RL2, L2 = forall_gs(pw, a, wl, alpha, g1s, mode=0)
+    
+    sl = Lattice(pw, a, wl, kpinc, rotation=alpha) + Lattice(pw, a, wl, kpinc, rotation=0)
+
+    S1i = redheffer_product(S1, Si)
+    Stot = redheffer_product(S1i, S2)
+
+    c1p = compute_source(sl, wl)
+    # Propagate the source in the whole stack
+    c1m, c2p = Stot[0,0] @ c1p, Stot[1,0] @ c1p
+    # Do that again but target one specific layer (S2)
+    cdplus, cdminus = translate_mode_amplitudes(S1i, c1p, c1m)
+    fourier_fields = fourier_fields_from_mode_amplitudes((np.block([list(r) for r in RL2]), np.diag(L2)), np.block([list(r) for r in R02]), (cdplus, cdminus), 0.2*a*sl.k0)
+    #real_fields = [ fourier2direct(ff.reshape(sl.pw), 0*a, target_resolution=(127,127), kp=(0,0)) for ff in fourier_fields ]
+    real_fields = [ fourier2direct2(ff.reshape(sl.pw), sl.kx, sl.ky, 12 *a, target_resolution=(127,127), kp=(0,0)) for ff in fourier_fields ]
+    fig, axs = plt.subplots(2,2)
+    for i, ax in enumerate(axs.flat):
+        ax.matshow(real_fields[i].real, cmap="RdBu")
+    plt.show()
+    #Sr, Wr = scattering_reflection(sl.KX, sl.KY, sl.W0, sl.V0)
+    #St, Wt = scattering_transmission(sl.KX, sl.KY, sl.W0, sl.V0)
+    #Stot = redheffer_product(Stot, Sr)
+    #Stot = redheffer_product(St, Stot)
+    return 0,0
+
+if __name__ == '__main__':
+    #fs = 0.803*c/a
+    #angle = np.deg2rad(18)
+    #fs = 0.76*c/a
+    #angle = np.deg2rad(30)
+    fs = args.freq*c/a
+    angle = np.deg2rad(args.angle)
+    T,R = compute_field_slice(-angle, fs)
--- examples/fan_extension/twisted_map2.py
+++ examples/fan_extension/twisted_map2.py
@@ -10,22 +10,18 @@ parser.add_argument("-batch_size", type=int, default=64)
 parser.add_argument("-polar", type=str, default="RCP")
 args = parser.parse_args()
 
-from bast.alternative import scattering_structured_layer, Lattice, scattering_identity, scat_base_transform,redheffer_product,incident,scattering_reflection,scattering_transmission, scattering_uniform_layer, poynting_fluxes
+from bast.alternative import scattering_structured_layer, Lattice, scat_base_transform,redheffer_product,incident,scattering_reflection,scattering_transmission, scattering_uniform_layer, poynting_fluxes
 from bast.tools import c
 import numpy as np
-from bast.tools import joint_subspace, rotation_matrix
-from numpy.lib.scimath import sqrt as csqrt
+from bast.tools import joint_subspace
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from itertools import product
-from cmath import sqrt as csqrt
-from scipy.linalg import block_diag
-from numpy.linalg import inv, solve
 from bast.misc import block_split
 
 
-pw = (3,3)
-a = 1e-2
+pw = (5,5)
+a = 1
 kpinc = (0,0)
 
 # Define the structure
@@ -36,25 +32,32 @@ y = np.linspace(-0.5, 0.5, N, endpoint=True)
 X, Y = np.meshgrid(x, y)
 eps[np.sqrt(X**2+Y**2)<= 0.25] = 1.0
 
-def get_smatrix(l, wl):
-    S = scattering_structured_layer(l, eps, 0.2 * a)
-    Si = scattering_uniform_layer(l, 1.0, 0.3 * a)
-    S = redheffer_product(S, Si)
-    #S = scat_base_transform(S, l.W0)
-    # Convert [2,2,N,N] to list to [2N,2N]
+def get_layer_smatrix(l):
+    S, WL, VL, LL = scattering_structured_layer(l, eps, 0.2 * a, return_eigenspace=True)
+    #Si = scattering_uniform_layer(l, 1.0, 0.3 * a)
+    #S = redheffer_product(S, Si)
     S = np.asarray(np.bmat([ list(s) for s in list(S) ]))
-    return S, l.W0
+    return S, WL, VL, LL, l.W0
+def get_interlayer_smatrix(l):
+    S, WL, VL, LL = scattering_uniform_layer(l, 1.0, 0.3 * a, return_eigenspace=True)
+    S = np.asarray(np.bmat([ list(s) for s in list(S) ]))
+    return S, WL, VL, LL, l.W0
 
-
-def forall_gs(pw, a, wl, alpha, gs):
+def forall_gs(pw, a, wl, alpha, gs, mode=None, interlayer=False):
     Ss = list()
     W0s = list()
     for kp in gs.T:
         l = Lattice(pw, a, wl, kp, rotation=alpha)
-        Sl, W0 = get_smatrix(l, wl)
+        if not interlayer:
+            Sl, WL, VL, LL, W0 = get_layer_smatrix(l)
+        else:
+            Sl, WL, VL, LL, W0 = get_interlayer_smatrix(l)
         Ss.append(Sl)
         W0s.append(W0)
-    return Ss, W0s
+    
+    bW0 = joint_subspace([np.tile(w, (2,2)) for w in W0s], mode)
+    bW0 = block_split(bW0)
+    return block_split(joint_subspace(Ss, mode)), bW0[0,0]
 
 
 
@@ -71,6 +74,8 @@ def compute_fluxes(sl, S11, S21, wl):
         esrc = incident(sl.pw, 1, 0, kp=(sl.kp[0], sl.kp[1], kzi))
     elif args.polar == "P":
         esrc = incident(sl.pw, 0, 1, kp=(sl.kp[0], sl.kp[1], kzi))
+    elif args.polar == "SP":
+        esrc = incident(sl.pw, 1, 1, kp=(sl.kp[0], sl.kp[1], kzi))
     else:
         assert(False)
 
@@ -81,25 +86,14 @@ def transmission_tot(alpha, f):
     g1s = Lattice(pw, a, wl, kpinc, rotation=0).g_vectors
     g2s = Lattice(pw, a, wl, kpinc, rotation=alpha).g_vectors
     
-    S1s, W10s = forall_gs(pw, a, wl, 0, g2s)
-    S2s, W20s = forall_gs(pw, a, wl, alpha, g1s)
+    S1, W10s = forall_gs(pw, a, wl, 0,     g2s, mode=1)
+    Si, Wi0s = forall_gs(pw, a, wl, alpha, g1s, mode=1, interlayer=True)
+    S2, W20s = forall_gs(pw, a, wl, alpha, g1s, mode=0)
     
-    S1 = joint_subspace(S1s, 1)
-    S2 = joint_subspace(S2s, 0)
-    # W0 @ S1 @ W0' @ W0 @ S2 @ W0'
-    S1 = block_split(S1)
-    bW0 = joint_subspace([np.tile(w, (2,2)) for w in W10s], 1)
-    bW0 = block_split(bW0)
-    scat_base_transform(S1, bW0[0,0])
-
-    S2 = block_split(S2)
-    bW0 = joint_subspace([np.tile(w, (2,2)) for w in W20s], 1)
-    bW0 = block_split(bW0)
-    scat_base_transform(S2, bW0[0,0])
-
-
     sl = Lattice(pw, a, wl, kpinc, rotation=alpha) + Lattice(pw, a, wl, kpinc, rotation=0)
-    Stot = redheffer_product(S1, S2)
+
+    Stot = redheffer_product(S1, Si)
+    Stot = redheffer_product(Stot, S2)
     #Sr, Wr = scattering_reflection(sl.KX, sl.KY, sl.W0, sl.V0)
     #St, Wt = scattering_transmission(sl.KX, sl.KY, sl.W0, sl.V0)
     #Stot = redheffer_product(Stot, Sr)
@@ -108,20 +102,22 @@ def transmission_tot(alpha, f):
 
 if __name__ == '__main__':
     fs = np.linspace(0.7 * c / a, 0.84 * c/a, args.nfreqs)
-    angles = np.linspace(1e-2, 45, args.nangles)
+    #fs = np.linspace(0.9 * c / a, 2 * c/a, args.nfreqs)
+    angles = np.linspace(1e-2, 90, args.nangles)
+    #angles = [6.72]
     angles = np.deg2rad(angles)
     Ts, Rs = list(), list()
     awl = np.asarray(list(product(fs, angles)))
     if args.id >= 0:
         work = awl[args.id * args.batch_size:(args.id+1)*args.batch_size]
         for f, angle in work:
-            T,R = transmission_tot(-angle, f)
+            T,R = transmission_tot(angle, f)
             Rs.append(R)
             Ts.append(T)
-        np.savez(f"{args.polar}_3x3/RT_{args.id}.npy", T=Ts, R=Rs)
+        np.savez(f"{args.polar}_{pw[0]}x{pw[1]}/RT_{args.id}.npz", T=Ts, R=Rs)
     else:
         for f,angle in tqdm(awl):
             T, R = transmission_tot(angle, f)
             Ts.append(T)
             Rs.append(R)
-        np.savez(f"{args.polar}_3x3_RT.npz", T=Ts, R=Rs)
+        np.savez(f"{args.polar}_{pw[0]}x{pw[1]}_RT.npz", T=Ts, R=Rs)
