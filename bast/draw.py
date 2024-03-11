430ccece6721218cd4e4580fe2d00acf2c59a344
Kaeryv <kaeryv.alden@gmail.com>
2024-03-11 15:28:54 +0100

    Drawing rectangles

--- bast/draw.py
+++ bast/draw.py
@@ -11,12 +11,22 @@ class Drawing:
         self.x0, self.y0, self.x1, self.y1 = -0.5, -0.5, 0.5, 0.5
         self.xaxis = np.linspace(self.x0, self.x1, shape[0], endpoint=True)
         self.yaxis = np.linspace(self.y0, self.y1, shape[1], endpoint=True)
-        self.X, self.Y = np.meshgrid(self.xaxis, self.yaxis)
+        self.X, self.Y = np.meshgrid(self.xaxis, self.yaxis, indexing="ij")
     
 
     def circle(self, xy, radius, epsilon):
         x, y = xy
         self._canvas[np.sqrt((self.X-x)**2+(self.Y-y)**2) <= radius] = epsilon
+    
+    def rectangle(self, xy, wh, epsilon):
+        x, y = xy
+        w, h = wh
+        x = x - w / 2
+        y = y - h / 2
+        xmask = (self.X >= x) & (self.X <= x + w)
+        ymask = (self.Y >= y) & (self.Y <= y + h)
+        self._canvas[xmask & ymask] = epsilon
+
 
     def canvas(self, shape=None, interp_method="linear"):
         if shape is None:
