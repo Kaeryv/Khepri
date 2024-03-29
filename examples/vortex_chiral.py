e118bbf98e54de0338760fc5b2e69c645c506e2b
Kaeryv <kaeryv.alden@gmail.com>
2024-03-29 15:53:13 +0100

    Nice vortex chiral sweep to do

--- examples/vortex_chiral.py
+++ examples/vortex_chiral.py
@@ -19,33 +19,28 @@ from bast.tools import reciproc
 twist_angle=float(sys.argv[3])
 
 
-def moire_lattice(angle_deg):
-    angle = np.deg2rad(angle_deg)
-    R = rot(np.pi/2)
-    R1 = rot(angle/2)
-    R2 = rot(-angle/2)
-    #AM = 0.5 / np.sin(angle/2)* R @ np.eye(2)
-    #A = R1 @ R1 @ np.eye(2)
-    AM = np.linalg.inv(R1-R2) @ np.eye(2)
-    return AM
-
 def size(angle_deg):
+    angle = np.deg2rad(angle_deg)
     #AM = moire_lattice(angle_deg)
     #return np.linalg.norm(AM[0])
-    return 1/((1/np.cos(np.deg2rad(angle_deg)))-1)
+    return float(sys.argv[4])/((1/np.cos(angle))-1)
+    #return float(sys.argv[4])/np.sqrt(1-np.cos(2*angle))
+
+def moire_lattice(angle_deg):
+    angle = np.deg2rad(angle_deg)
+    #R = rot(np.pi/2)
+    #R1 = rot(angle/2)
+    #R2 = rot(-angle/2)
+    #AM = 0.5 / np.sin(angle/2)* R @ np.eye(2)
+    #A = R1 @ R1 @ np.eye(2)
+    #AM = np.linalg.inv(R1-R2) @ np.eye(2)
+    AM = rot(angle/2)@ np.eye(2)
+    AM *= size(angle_deg)
+    return AM
+
 twisted = twist_angle != 0
-#cell_size = 15.38 + 0*size(twist_angle) if twisted else 1
 cell_size =  size(twist_angle) if twisted else 1
 print(cell_size)
-#cell_size = 15.3
-#print(cell_size)
-#exit()
-
-if twisted:
-    moire_basis = moire_lattice(twist_angle)
-    moire_reciproc = reciproc(moire_basis[0], moire_basis[1]) 
-else:
-    moire_reciproc=None
 
 bzi = int(sys.argv[1])
 
@@ -54,26 +49,14 @@ kbz = gen_bzi_grid((bzi, bzi), a=cell_size, reciproc=None).reshape(2, -1).T
 NS = 31 #int(cell_size * 3)
 wl = 1/0.725#1 / 0.5
 theta = 0.0
-x = np.linspace(0, cell_size*bzi, NS*bzi, endpoint=True)
-y = np.linspace(0, cell_size*bzi, NS*bzi, endpoint=True)
-X, Y = np.meshgrid(x,y, indexing="ij")
-Z = np.zeros_like(X)
 
-#kp = compute_kplanar(1, wl, theta, 0)
-#kbz[:, 0] += kp[0].real
-#kbz[:, 1] += kp[1].real
+'''
+    Build and solve the crystal
+'''
 
-
-#source_real=shifted_rotated_fields(_paraxial_gaussian_field_fn, X, Y, Z, wl, np.max(x)/2, np.max(y)/2, -3,theta,0.0*np.pi,0)
-source_real = shifted_rotated_fields(_paraxial_laguerre_gaussian_field_fn, X, Y, Z, wl, np.max(x)/2, np.max(y)/2, 0, theta, 0, np.pi/4, l=1, p=0, w0=1.2*cell_size)
-
-source_real = np.asarray(source_real)
-source_real = np.swapaxes(source_real, 0, 2)
-source_real = np.swapaxes(source_real, 1, 3)
-
-pattern = Drawing((128,128), 12)
-#pattern.circle((0,0), 0.25, 1)
-pattern.rectangle((0,0), (1,0.5), 1)
+pattern = Drawing((128,128), 4)
+pattern.circle((0,0), 0.25, 1)
+#pattern.rectangle((0,0), (1,0.5), 1)
 pwx = int(sys.argv[2])
 pw = (pwx, pwx)
 def get_crystals(kbz, twisted=False):
@@ -82,27 +65,30 @@ def get_crystals(kbz, twisted=False):
         e = Expansion(pw)
         for kp in kbz:
             cl = Crystal(pw, epse=1)
-            ss = 1
-            cl.add_layer_uniform("S1", 1, 0.3/ss)
+            ss = 2
+            cl.add_layer_uniform("S1", 1, 0.7/ss)
             cl.add_layer_pixmap("Scyl", pattern.canvas(), 0.4)
-            cl.add_layer_uniform("S2", 1, 1/ss)
-            cl.set_stacking(["S1","Scyl", "S2"])
+            cl.add_layer_uniform("S2", 1, 0.7/ss)
+            cl.set_stacking(["Scyl"])
             cl.set_source(wl, np.nan, np.nan, kp=kp)
             cl.solve()
             crystals.append(cl)
     else:
         e1, e2 = Expansion(pw), Expansion(pw)
         e2.rotate(twist_angle)
+        #e1.rotate(-twist_angle/2)
         etw = e1 + e2
+        #etw._g_vectors = rot(-np.deg2rad(twist_angle)/2) @ etw.g_vectors
         for kp in tqdm(kbz):
             cl = Crystal.from_expansion(etw)
-            cl.a = cell_size
-            cl.add_layer("Sref",  EL(etw, Layer.half_infinite(e1, "reflexion", 1),   e2.g_vectors, 1))
-            cl.add_layer("S1",    EL(etw, Layer.pixmap(e1, pattern.canvas(), 0.4),   e2.g_vectors, 1))
-            cl.add_layer("Si",    EL(etw, Layer.uniform(e1, 1.0,  0.3),              e2.g_vectors, 1))
-            cl.add_layer("S2",    EL(etw, Layer.pixmap(e2, pattern.canvas(), 0.4),   e1.g_vectors, 0))
-            cl.add_layer("Strans",EL(etw, Layer.half_infinite(e2, "transmission", 1),e1.g_vectors, 0))
-            cl.set_stacking(["S1", "Si", "S2"])
+            #cl.a = cell_size
+            cl.add_layer("Sref",  EL(etw, Layer.half_infinite(e1, "reflexion", 1),    e2.g_vectors, 1))
+            cl.add_layer("S1",    EL(etw, Layer.pixmap(e1, pattern.canvas(), 0.4),    e2.g_vectors, 1))
+            cl.add_layer("Si",    EL(etw, Layer.uniform(e1, 1.0,  0.3),               e2.g_vectors, 1))
+            cl.add_layer("Si2",   EL(etw, Layer.uniform(e2, 1.0,  0.3),               e1.g_vectors, 0))
+            cl.add_layer("S2",    EL(etw, Layer.pixmap(e2, pattern.canvas(), 0.4),    e1.g_vectors, 0))
+            cl.add_layer("Strans",EL(etw, Layer.half_infinite(e2, "transmission", 1), e1.g_vectors, 0))
+            cl.set_stacking([ "S1", "S2"])
             cl.set_source(wl, np.nan, np.nan, kp=kp)
             cl.solve()
             crystals.append(cl)
@@ -111,9 +97,25 @@ def get_crystals(kbz, twisted=False):
 
 crystals, expansion = get_crystals(kbz, twisted=twisted)
 
+'''
+    Sources computation
+'''
+x = np.linspace(0, cell_size*bzi, NS*bzi, endpoint=True)
+y = np.linspace(0, cell_size*bzi, NS*bzi, endpoint=True)
+X, Y = np.meshgrid(x,y, indexing="ij")
+Z = np.zeros_like(X)
+
+zmax = crystals[0].stack_positions[-2]
+print(f"Computing sources {zmax=}")
+#source_real=shifted_rotated_fields(_paraxial_gaussian_field_fn, X, Y, Z, wl, np.max(x)/2, np.max(y)/2, -zmax/2,theta,0.0*np.pi,0)
+source_real = shifted_rotated_fields(_paraxial_laguerre_gaussian_field_fn, X, Y, Z, wl, np.max(x)/2, np.max(y)/2, -1, theta, 0, np.pi/4, l=1, p=0, w0=cell_size)
+
+source_real = np.asarray(source_real)
+source_real = np.swapaxes(source_real, 0, 2)
+source_real = np.swapaxes(source_real, 1, 3)
+
 from bast.beams import amplitudes_from_fields
 Fs = list()
-print("Computing sources")
 for kp in tqdm(kbz):
      Fs.append(amplitudes_from_fields(source_real, expansion, wl, kp, X, Y, (bzi, bzi), a=cell_size))
 Fs=np.asarray(Fs)
@@ -131,7 +133,7 @@ x, y = np.meshgrid(x, y, indexing="ij")
 print("Computing fields")
 fields = list()
 for c, F, kp in zip(tqdm(crystals), Fs, kbz):
-    E, H = c.fields_coords_xy(x, y, 0.1, F, use_lu=False)
+    E, H = c.fields_coords_xy(x, y, zmax+0.1, F, use_lu=False)
     fields.append((E, H))
 fields = np.asarray(fields)
 np.savez_compressed("tw_fields.npz", fields)
@@ -163,4 +165,4 @@ image = axs[1,1].matshow(np.angle(sr), cmap="hsv",extent=[0, np.max(x), 0, np.ma
 #ax1.set_aspect(ar)
 axs[0,0].set_xlabel("Length [µm]")
 axs[0,0].set_ylabel("Width [µm]")
-fig.savefig("vortex_twisted.png")
+fig.savefig("vortex_chiral.png")
